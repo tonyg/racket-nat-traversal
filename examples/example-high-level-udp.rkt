35dd40c992327ff32503d2cbd5207c17bdabb4a7
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2013-04-01 22:46:49 -0400

    UDP example.

---
+++ examples/example-high-level-udp.rkt
@@ -0,0 +1,26 @@
+#lang racket
+
+(require "../high-level.rkt")
+
+(define (handle-mapping-change assignments)
+  (pretty-print `(assignments . ,(set->list assignments))))
+
+(define s (udp-open-socket))
+(define mapping-manager (udp-bind!/public s #f 5999 #:on-mapping-change handle-mapping-change))
+
+(define buffer (make-bytes 65536))
+(let loop ()
+  (sync (handle-evt (udp-receive!-evt s buffer)
+		    (match-lambda
+		     [(list packet-length remote-address remote-port)
+		      (match (string-trim (bytes->string/utf-8 (subbytes buffer 0 packet-length)))
+			["quit"
+			 (printf "Told to quit.\n")
+			 (mapping-change-listener-stop! mapping-manager)
+			 (sleep 0.5)
+			 (udp-close s)]
+			[packet
+			 (udp-send-to s remote-address remote-port
+				      (string->bytes/utf-8
+				       (format "You said: ~a\n" packet)))
+			 (loop)])]))))
